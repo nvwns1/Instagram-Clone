f28b7dc04e3f4f07f1bf13b175f0bf0d82ad6d89
nvwns1 <nvwns12345@gmail.com>
2024-04-02 15:09:34 +0545

    Refactor Button component to spread rest props

--- src/components/component/Button/Button.tsx
+++ src/components/component/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React, { ButtonHTMLAttributes } from 'react'
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-    label: string
+    label: string,
 }
 
-const Button = ({ label }: IButtonProps) => {
+const Button = ({ label, ...rest }: IButtonProps) => {
     return (
         <button
+            {...rest}
             className='bg-accent text-white py-1 w-full rounded-md text-sm font-semibold my-2'>
             {label}
         </button>
